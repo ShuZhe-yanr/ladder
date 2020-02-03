7dfd2cc03aa75deaa97ca4cfd79361d79001a563
e <466651364@qq.com>
2020-02-03 20:43:55 +0800

    project init

---
+++ Travel/src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>Home</div>
+    <router-link to="./List">list</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+
+</style>
